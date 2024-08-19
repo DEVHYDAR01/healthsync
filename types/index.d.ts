4bdce7220fb2f619e68b088e60df7cf17a9b7c19
Pecjane <promisebarth@gmail.com>
2024-08-19 20:20:34 +0000

    This commit will update the appointment

--- types/index.d.ts
+++ types/index.d.ts
@@ -37,3 +37,21 @@ declare interface RegisterUserParams extends CreateUserParams {
   identificationDocument: FormData | undefined;
   privacyConsent: boolean;
 }
+
+declare type CreateAppointmentParams = {
+  userId: string;
+  patient: string;
+  primaryPhysician: string;
+  reason: string;
+  schedule: Date;
+  status: Status;
+  note: string | undefined;
+};
+
+declare type UpdateAppointmentParams = {
+  appointmentId: string;
+  userId: string;
+  appointment: Appointment;
+  type: string;
+};
+ 
